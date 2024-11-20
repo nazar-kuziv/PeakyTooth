949a685b79d04ed7e5a58569871f38eb80c928c3
nazar-kuziv <n.kuziv2005@gmail.com>
2024-11-20 21:03:17 +0100

    Fix login

---
+++ view/screen_main.py
@@ -0,0 +1,26 @@
+from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QMainWindow, QToolBar, QWidget
+
+from view.widget.button_base import ButtonBase
+
+
+class ScreenMain(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.set_menu_bar()
+
+    def set_menu_bar(self):
+        toolbar = QToolBar()
+        toolbar.setMovable(False)
+        toolbar.setMinimumHeight(50)
+        # noinspection PyUnresolvedReferences
+        self.addToolBar(Qt.TopToolBarArea, toolbar)
+
+        spacer = QWidget()
+        # noinspection PyUnresolvedReferences
+        spacer.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        toolbar.addWidget(spacer)
+
+        btn = ButtonBase('Logout')
+        toolbar.addWidget(btn)
