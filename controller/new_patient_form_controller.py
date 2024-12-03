28bd9da1ce8202d68d8bad3d8f8574e0d9882c98
verba05 <verba447@gmail.com>
2024-12-04 00:05:55 +0100

    add search patient function

--- controller/new_patient_form_controller.py
+++ controller/new_patient_form_controller.py
@@ -16,18 +16,40 @@ class NewPatientFormController:
         except DBUnableToConnect as e:
             self.view.show_error(e)
 
-    def add_new_patient(self, name, surname, date_of_birth, sex,  email, telephone, analgesics_allergy):
-        if name == '' or surname == '':
+    import re
+    from utils.user_session import UserSession
+
+    def add_new_patient(self):
+        # Extract data from UI fields
+        name = self.view.name_field.text()
+        surname = self.view.surname_field.text()
+        date_of_birth = self.view.dob_field.text()
+        sex = self.view.sex_field.currentText()
+        email = self.view.email_field.text()
+        telephone = self.view.telephone_field.text()
+        analgesics_allergy = self.view.allergy_checkbox.isChecked()
+
+        # Validate inputs
+        if not name or not surname:
             self.view.show_message('Name and surname fields should not be empty')
             return
+
         email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-        if re.match(email_regex, email) is None:
+        if not re.match(email_regex, email):
             self.view.show_message('Invalid Email Address')
             return
+
         phone_regex = r'^\+?[\d\s\(\)-]{7,15}$'
-        if re.match(phone_regex, telephone) is None:
+        if not re.match(phone_regex, telephone):
             self.view.show_message('Invalid Phone Number')
             return
+
+        # Add patient to database
         user_session = UserSession()
-        message = self.db.addNewPatient(name, surname, date_of_birth, sex, email, telephone, analgesics_allergy, user_session.organization_id)
+        message = self.db.addNewPatient(
+            name, surname, date_of_birth, sex, email, telephone, analgesics_allergy, user_session.organization_id
+        )
+
+        # Show confirmation message
         self.view.show_message(message)
+
